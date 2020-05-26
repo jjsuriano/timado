22270f8aff2430fa858eda708c10c28453451b30
Julio Suriano <julios@vt.edu>
2020-05-26 16:00:24 -0600

    Setup socket.io

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // REQUIRE
 const express = require("express");
+const socket = require("socket.io");
 
 // APP SETUP
 const app = express();
@@ -10,3 +11,11 @@ const server = app.listen(PORT, () => {
 
 // STATIC FILES
 app.use(express.static('public'));
+
+// SOCKET SETUP
+const io = socket(server);
+
+io.on("connection", (socket) => {
+    console.log("Socket connection made");
+    console.log("Users connected: " + socket.client.conn.server.clientsCount);
+});
---
+++ public/game.js
@@ -0,0 +1,3 @@
+// MAKE SOCKET CONNECTIOn
+const socket = io.connect('http://localhost:4000');
+
