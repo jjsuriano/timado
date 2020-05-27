65e173b60d5d823f1f04005559f15fb93c4ea377
Julio Suriano <julios@vt.edu>
2020-05-26 20:22:51 -0600

    different game room setup

--- client/public/js/game.js
+++ client/public/js/game.js
@@ -1,22 +1,53 @@
 // MAKE SOCKET CONNECTION
-const socket = io.connect('http://localhost:4000');
-
+const socket = io.connect('http://localhost:3000');
 
 // QUERY DOM
-let answers = document.getElementById("answers");
-let answer = document.getElementById("answer");
-let send = document.getElementById("send");
+const form = document.getElementById("game-form")
+const answers = document.getElementById("answers");
+const answer = document.getElementById("answer");
+const send = document.getElementById("send");
+const gameName = document.getElementById("game-name");
+const usersList = document.getElementById("users");
 
 // EMIT EVENT
-send.addEventListener("click", () => {
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
     socket.emit("answer", {
         answer: answer.value
     });
     answer.value = "";
 });
 
+
+// GET NAME AND ROOM FROM URL
+const {name, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+});
+
+socket.emit("joinRoom", {name, room});
+
 // LISTEN FOR EVENTS
 socket.on("answer", (data) => {
-    console.log(data.answer)
     answers.innerHTML += "<p>" + data.answer + "</p>";
+
 });
+
+socket.on("roomUsers", ({room, users}) => {
+    roomName(room);
+    outputUsers(users);
+
+})
+
+// USER CONNECT AND DISCONNECT
+socket.on("message", (data) => {
+    console.log(data);
+});
+
+function roomName(room) {
+    gameName.innerHTML = room;
+}
+
+function outputUsers(users) {
+    usersList.innerHTML = `${users.map(user => `<li>${user.name}</li>`).join("")}`;
+}
--- index.js
+++ index.js
@@ -1,25 +1,59 @@
 // REQUIRE
+const path = require("path");
 const express = require("express");
+const http = require("http")
 const socket = require("socket.io");
+
+const { joinUser, getCurrentUser,  userLeft, getRoomUsers } = require("./utils/users");
 
 // APP SETUP
 const app = express();
-const PORT = 4000;
-const server = app.listen(PORT, () => {
-    console.log(`Listening to requests on port ${PORT}...`);
-});
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
 
 // STATIC FILES
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname,"/client/public")));
 
 // SOCKET SETUP
 const io = socket(server);
 
 io.on("connection", (socket) => {
     console.log("Socket connection made");
-    console.log("Users connected: " + socket.client.conn.server.clientsCount);
+    console.log("Users connected: " + socket.client.conn.server.clientsCount + "\n");
 
-    socket.on("answer", (data) => {
-        io.sockets.emit("answer", data);
+    socket.on("joinRoom", ({ name, room }) => {
+        const user = joinUser(socket.id, name, room);
+        socket.join(user.room);
+
+        socket.on("answer", (data) => {
+            io.to(user.room).emit("answer", data);
+        });
+    
+    
+        socket.broadcast.to(user.room).emit("message", `${user.name} se acaba de unir al juego`);
+
+        io.to(user.room).emit("roomUsers", {
+            room: user.room,
+            users: getRoomUsers(user.room),
+        });
     });
+
+    socket.on("disconnect", () => {
+        console.log("Socket connection closed");
+        console.log("Users connected: " + socket.client.conn.server.clientsCount + "\n");
+        const user = userLeft(socket.id);
+        if (user) {
+            io.to(user.room).emit("message", `${user.name} se acaba de desconectar`);
+            io.to(user.room).emit("roomUsers", {
+                room: user.room,
+                users: getRoomUsers(user.room),
+            });
+        }
+    });
+});
+
+// SERVER
+
+server.listen(PORT, () => {
+    console.log(`Listening to requests on port ${PORT}...`);
 });
